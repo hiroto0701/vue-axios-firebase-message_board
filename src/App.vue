fbfeeb36c64d987ff9137df71773736c817dd15f
hiroto <hiroto.soccer.0701@gmail.com>
2023-07-25 15:30:31 +0900

    実装完了

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>掲示板</h1>
+    名前
+    <div>
+      <input type="text" v-model="name">
+    </div>
+    コメント
+    <div>
+      <textarea v-model="comment" cols="30" rows="10"></textarea>
+      <br>
+      <br>
+      <button @click="submitPosts">投稿する</button>
+      <br>
+      <br>
+      <h2>投稿一覧</h2>
+      <div v-for="post in posts" :key="post.name">
+        <hr>
+        <p>名前：{{ post.fields.name.stringValue }}</p>
+        <p>コメント：{{ post.fields.comment.stringValue }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: '',
+      comment: '',
+      posts: ''
+    }
+  },
+  methods: {
+    submitPosts() {
+      axios.post(
+        "https://firestore.googleapis.com/v1/projects/vue-test-63736/databases/(default)/documents/posts",
+        {
+          fields : {
+            name: {
+              stringValue: this.name
+            },
+            comment: {
+              stringValue: this.comment
+            }
+          }
+        }
+      ).then(() => {
+        this.name = '';
+        this.comment= '';
+        this.getPosts();
+      });
+    },
+    getPosts() {
+      axios.get(
+        "https://firestore.googleapis.com/v1/projects/vue-test-63736/databases/(default)/documents/posts",
+      ).then(res => {
+        this.posts = res.data.documents;
+      });
+    }
   }
 }
 </script>
